82edbb9b8912dcaa390d7fb14b5f58840fb6e136
Tom Powell <zifnab@zifnab06.net>
2013-11-26 19:05:43 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+BungeePingList
+===========
+
+Configurable user ping list for Bungee
+
+Non-forced hosts will show the following:
+
+    server1: 4/50
+    server2: 400/500
+    server3: 1/50
+
+Forced hosts will show the normal user count. 
---
+++ src/at/junction/bungee/pinglist/BungeePingList.java
@@ -0,0 +1,15 @@
+package at.junction.bungee.query;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+//Hacked together by zifnab, please ignore the shitty code
+public class BungeeQuery extends Plugin {
+    @Override
+    public void onEnable() {
+		super.onEnable();
+	}
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+}
